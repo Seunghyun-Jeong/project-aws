1112e8ff323ca8ad3548f370b5d7fe07a3b7ac41
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-06-10 18:30:47 +0900

    add .js

---
+++ fastify/plugins/monngdb.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const fp = require('fastify-plugin')
+
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(require('@fastify/mongodb    '), {
+    forceClose: true,
+  
+    url: 'mongodb://mongo/mydb'
+  })
+})
