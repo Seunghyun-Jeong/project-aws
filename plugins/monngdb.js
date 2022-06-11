58e4cd3c5d4a548ca377f613380a9e6f0be5d1ca
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-06-12 01:16:26 +0900

    mongodb.js 수정

--- plugins/monngdb.js
+++ plugins/monngdb.js
@@ -3,7 +3,7 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('@fastify/mongodb    '), {
+  fastify.register(require('@fastify/mongodb'), {
     forceClose: true,
   
     url: 'mongodb://mongo/mydb'
