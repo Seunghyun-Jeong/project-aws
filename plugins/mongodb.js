eb13ed02ac88956d4c4b9197f475df1660781563
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-06-13 14:37:25 +0900

    ADD:mongodb.js & cors.js

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -5,5 +5,5 @@ const fp = require('fastify-plugin')
 module.exports = fp(async function (fastify, opts) {
 fastify.register(require('@fastify/mongodb'), {
   forceClose: true,
-  url: 'mongodb://mongodb://admin:admin@localhost:27017/:27017'
+  url: 'mongodb://admin:admin@localhost:27017/baedal'
 })})
