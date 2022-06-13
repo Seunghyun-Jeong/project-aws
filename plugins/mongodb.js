34b6906a430aca5b97485ab759b7fb8c3173131c
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-06-13 15:56:39 +0900

    mongodb url 수정

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -5,5 +5,5 @@ const fp = require('fastify-plugin')
 module.exports = fp(async function (fastify, opts) {
 fastify.register(require('@fastify/mongodb'), {
   forceClose: true,
-  url: 'mongodb://admin:admin@localhost:27017/baedal'
+  url: 'mongodb://admin:admin@project2-nlb-3260743723a58d27.elb.ap-northeast-2.amazonaws.com:27017/baedal'
 })})
