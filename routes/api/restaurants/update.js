28f9f64f3cce4667232563554b7ae84d670b04a0
Seunghyun-Jeong <dkskakdgka@gmail.com>
2022-06-14 13:01:33 +0900

    url 수정

--- routes/api/restaurants/update.js
+++ routes/api/restaurants/update.js
@@ -2,9 +2,18 @@
 const { ObjectId } = require('@fastify/mongodb')
 
 module.exports = async function(fastify, opts) {
-    fastify.get('/', async function (req, reply) {
+    fastify.patch('/', async function (req, reply) {
         let id = process.env.RESTAURANT_ID
         const restaurants = this.mongo.db.collection('restaurants')
+        const query = {
+        '_id': this.mongo.ObjectId(id)
+        }
+        const doc = {
+        '$set': req.body
+        }
+
+        await restaurants.updateOne(query, doc)
+
         const result = await restaurants.findOne({ '_id': this.mongo.ObjectId(id) })
         return result
   })
